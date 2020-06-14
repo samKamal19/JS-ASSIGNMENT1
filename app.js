c8d105a1cf45be9e698b4107b53ae54f2d03c8f5
samkamal19 <samrakamal151@gmail.com>
2020-06-14 18:22:18 +0500

    JS Assignment 1-20

---
+++ app.js
@@ -0,0 +1,735 @@
+                     //****"JS- ASSIGNMENT"****//
+                          
+// ASSIGNMENT#1     ALERT
+
+//task#1) 
+{/* <script> */}
+    var a = " samrakamal1990@gmail.com";
+    alert( a );
+// </script>
+
+//task#2)  
+{/* <script> */}
+var a = " Error! please enter a valid passward";
+alert( a );
+// </script>
+
+//task#3) 
+
+var a =" Welcome to JS land" <br> "Happy Coding!";
+document.write( a );
+
+//task#4
+// {/* <script> */}
+var a = "Welcome to JS land";
+var b = "Happy coding";
+alert( a );
+alert( b );
+// </script>
+
+//task#5) 
+{/* <script> */}
+var a = " Hello.... i can run JS through my web browser console";
+document.write( a );
+// </script>
+
+//task#6)  
+//<html>
+//<head>
+//<link rel="stylesheet"  href="style.css">
+//</head>
+//<body>
+//<script>
+var a = "This is my HTML page";
+alert(a);
+//</script>
+//</body>
+//</html>
+
+//task#7)     
+//<html>
+//<head>
+//< link  rel="stylesheet"  href="style.css">
+//<script>
+var a = " This is my HTML page";
+alert ( a );
+//</script>
+//</head>
+//<script>
+var a = " This is my HTML page";
+alert ( a );
+//</script> 
+//<body>
+//<script>
+var a = " This is my HTML page";
+alert ( a );
+//</script> 
+//</body>
+//<script>
+var a = " This is my HTML page";
+alert ( a );
+//</script> 
+//</html>
+
+{/* ASSIGNMENT#2  VARIABLE FOR STRING */}
+
+{/* task#1)     */}
+ var username;
+
+{/* task#2)  */}
+ var myName=" samra kamal";
+
+{/* task#3)  */}
+ var message="Hello World";
+alert( message );
+
+{/* task#4)  <script> */}
+var studentName=" Jhon Doe";
+var age="15 years old";
+var certificate="certified mobile application develpement";
+alert( studentName);
+alert( age );
+alert( certificate );   
+{/* </script>     */}
+
+{/* task#5)   */}
+ var a = "pizza\r\npizz\r\npiz\r\npi\r\np";   
+alert( a ); 
+
+{/* task#6)  */}
+  var email="samrakamal1990@gmail.com" ;
+var a="My email address is "   +   email;
+alert(a);   
+
+{/* task#7)   */}
+var book="A smarter way to learn JavaScript";
+var a="I am trying to learn from book "   +   book;
+alert(a);
+
+{/* task#8)  <script> */}
+var a="Yah! i can write HTML content through JavaScript";
+document.write(a);
+{/* </script> */}
+
+//task#9)  
+
+
+
+
+
+
+
+
+
+
+//ASSIGNMENT#3     VARIABLE FOR NUMBER
+
+task#1) 
+ var age=" I am 15 years old ";
+alert(age);
+
+task#2)  var N ="16";
+alert( "you have visited this cites"        +          N        +        "times"  );
+
+
+task#3)  var birthYear = "1992";
+document.write ( "My birth year is"     +      birthYear );    
+document.write ( Dat type of my decleraed variable is number);
+
+task#4)  var a = "John Doe";
+var b = "XYZ clothing store";
+var c = "5 T- Shirts";
+document.write (    a       +         "ordered"         +       b    +        c     );
+
+
+
+
+//ASSIGNMENT#4     LEGAL N ILLEGAL
+
+
+task#1)    var a = " red, yellow, green ";
+
+task#2)   fruits , myVar  , $100   , hello_world  , carName
+
+birth date,  23 ,  19balls,  schoolname,  "age"
+
+task#3) 
+//<html>
+//<body>
+
+a)               <h1>Rules for naming JavaScript</h1>
+b)         <p> variable name can only contain, number, $ and underscores  . for example: $my 1st_variable</p>
+c)         <p>variable must begin with a dollar sign, letter or underscore. for example: $name, _name or name </p>
+d)        <p>variable name are case sensitive</p>
+e)         <p>variable name should not be a JS keyword</p>
+
+</body>
+</html>
+
+
+ASSIGNMENT#5     MATHS EXPRESSION
+
+task#1)   
+var a = 5;
+var b = 6;
+var c = a + b;
+document.write(c);
+
+task#2) 
+var a = 5;
+var b = 6;
+var c = a - b;
+document.write(c);
+
+var a = 5;
+var b = 6;
+var c = a * b;
+document.write(c);
+
+var a = 5;
+var b = 6;
+var c = a / b;
+document.write(c);
+
+task#3)  
+a) var a
+b) document.write(value after value declaration is:  expression);
+c) var a= 5;
+d) document.write( initial vale : 5);
+e) var b = a++;
+f) document.write( value after increment is : 6);
+g) var c= 6 + 7
+h)  document.write( value after addition is : 13);
+i) var d = c--;
+j) document.write( value after decrement is : 12);
+k) var e = d%3;
+l) document.write( the remainder is : 0);
+
+task#4)  <script>
+var movieTicketPrice = 600 PKR;
+var a = movieTicketPrice * 5;             / / 3000 PKR
+document.write( "total cost to buy 5 tickets to a movie is "   +    a  );
+</script>
+
+task#5) 
+var i = 4;
+for ( var i = 1; i <= 10; i++ ) {
+console.log( "4"  +   "x"   +    i     +     "="     +    4*i    +     "<br>");
+}
+
+task#6) 
+var celTemp = 25oC;    // f= 77oC
+var fahrenheitTemp = 70oC;    //c= 21.11111111oC
+document.write( 25oC to 77oC);
+document.write( 70oC to 21.111111111oC);
+
+task#7)  
+<h1>Shopping Cart</h1>
+
+//<script>
+
+var a = "Price of item 1  is 650" ;
+
+var b ="quanty of item 1  is  3" ;
+
+var c = "Price of item 2  is 100" ;
+
+var d = "quanty of item 2  is 7" ;
+
+var e = "Shipping charges is 100" ;
+
+document.write( a );
+document.write( b );
+document.write( c );
+document.write( d );
+document.write( e );
+
+//</script>
+
+//task#8) 
+<h1>Mark Sheet</h1>
+
+//<script>
+
+var a = " Total marks : 980" ;
+var b = " Marks obtain : 804" ;
+var percentage = 82.0408163265306%;
+document.write( a );
+document.write( b );
+document.write( percentage );
+
+//</script>
+
+
+task#9) 
+<h1> Currency in PKR</h1> 
+//<script>
+var a = 10 * 104.80;     // US $ to PKR
+var b = 25 * 28 ;           // riyal to PKR
+var c = a +b;
+document.write("Total currency in PKR : "     +    c);
+</script>
+
+task#10)   
+var a = 6;
+a)  var b = a + 5;
+b)  var c = b * 10;
+c)  var d = c / 2;
+        document.write(d);
+
+task#11)   
+<h1> Age calculator </h1>
+var a = 2016;
+var b = 1992;
+var c  = a - b;
+document.write("Your current year :"    +   a );
+document.write("Your Birth year :"    +   b );
+document.write( "Your age is"   +   c );
+c)            document.write("They are neither 24 or 25 years old");
+
+
+task#12)
+var radius = 20;
+var circumference = 2*3.14*20;      // 125.67
+var area = 3.14*400;     // 3.14*(20*20) = 1256.8
+document.write("Radius of circle: 20 ");  
+document.write("circumference of circle:  125.67");
+document.write("area of circle: 1256.8");
+
+task#13)  
+var favSnacks = chocolate chip;
+var currentAge = 15;
+var maxAge = 65;      // 65-15 = 50
+var amontPerDay = 3;      // 50*3 = 150
+document.write("Favouite snacks is "  +  favSnacks);
+document.write("current age:"  +   currentAge);
+document.write("estimated maxmimum age"  +   maxAge);
+document.write("Amount of snacks is"   +  amontPerDay);
+document.write("You will need 150 chocolate chip to last you until the ripe old age is 65");
+
+
+ASSIGNMENT#6      MATH EXPRESSION
+
+task#1)
+var a = "the value of a:  10"; 
+var b = "the value of ++a is 11";
+var c = "the value of a++ : 11";
+var d = "the value of a-- : 11";
+var e = "the value of --a : 11";
+document.write( a );
+document.write( b  \r\n " NOw the value of a  is  11" );  
+document.write( c  \r\n "Now the value of a is  11 "); 
+document.write( d  \r\n "Now the value of a is  11 " );
+document.write( e \r\n "Now the value of a is  11 " );
+
+task#2)
+--a ;                              // 1
+--a   -   --b;                      //1
+--a   -   --b   +   ++b;        // 2
+--a   -   --b   +    ++b  +   b--;    //3  
+console.log ( "a is 1");
+console.log ( "b is 0");
+console.log ( "result is 3");
+
+task#3) 
+var a= prompt("user name");
+console.log(a);
+
+task#4)     question not mention
+
+task#5) 
+var i = prompt("user number");   
+var default = 5;
+for (  i = 1 ; i <11 ; i++); {
+document.write ( "user number"  +   "x"   +    i     +     "="     +    user number*i    +     "<br>");
+}   
+
+default {
+document.write( "5"  +   "x"   +    i     +     "="     +    5*i    +     "<br>");
+}
+
+
+task#6) 
+<table>
+
+var a = English;
+var b = Maths;
+var c = Urdu;
+var totalMarks = 100;
+
+</table>
+
+
+
+ASSSIGNMENT#6   USER INPUT AND CONDITIONAL STATEMENT  
+
+TASK#1)   
+var a = prompt("Input city name");
+if ( a === "karachi") {
+alert( "Welcome to city of lights");
+}
+
+task#2)  
+var a = prompt(" input your's Gender");
+if (a === "male") {
+alert( "Good Morning Sir");
+}   if ( a === "female") {
+alert( "Good Morning Ma'am");
+}
+
+task#3)
+var a = prompt("Input Traffic light");
+if (a ==="red") {
+alert("Must Stop");
+}  if (a === "yellow") {
+alert("Ready to move");
+}  if(a === "green") {
+alert(Move now);
+}
+
+task#4) 
+var a = prompt("Input remaining fuel in your car");
+if (a < 0.25 litres) {
+alert("Please refill the fuel in your car");
+}
+
+task#5) 
+a)  var a = 4;
+if(++a === 5) {
+alert("given condition for variable a is true");      // true
+}
+
+b)   var b = 82;
+if( b++ === 5) {
+alert("given condition for variable a is true");      // 
+}
+
+c)   var c = 12;
+if (c++ === 13) {
+alert(" condition 1 is true");      // false
+}
+if (c=== 13) {
+alert(" condition  2 is true");     // true
+}
+if (++c < 14 ) {
+alert(" condition 3 is true");      // false
+}
+if (c === 14) {
+alert(" condition 4 is true");     // true     
+}
+
+d)  var materialCost = 2000;
+var laborCost = 2000;
+var totalCost = materialCost  +  laborCost;
+if (totalCost === materialCost  +  laborCost) {
+alert("The cost equal");                  // true
+}
+
+e) if ("true") {
+alert("true");
+}
+if ("false") {
+alert("false");
+}
+
+f)  if("car" < "cat")  {
+alert ("car is smaller than cat");       // true
+}
+
+task#6)
+var a = 300;
+var b = 219;
+var  c = 73                //  219/300 * 100 = 73%
+
+alert( "total marks is  :  "     +     a   );
+alert( "obtained marks is  :  "     +      b   );
+alert( "Percentage is  : "      +     c    ); 
+
+if (  c <= 80 ) {
+alert( grade "A-one" , Remarks "Excellent");
+} if (  c <= 70 ) {
+alert( grade "A" , remarks"Good") ;
+}  if (  c <= 60 ) {
+alert(grade "B" , remarks "You need to improve") ;
+} if ( c >60 ) {
+alert( grade "Fail" , remarks "Sorry");
+}
+
+alert( "total marks is  :  "     +     a   );
+alert( "obtained marks is  :  "     +      b   );
+alert( "Percentage is  : "      +     c    );
+
+task#7)  
+var secretNumber = 5;
+var b = prompt("enter your number from 1 to 10");
+if (secretNumber = b ) {
+alert ("Bingo! correct answer");
+}  if (secretNumber  +1   = b)   {
+alert ("Close enogh to right amswer");
+}     
+
+task#8) 
+var a = prompt("enter a number");
+if ( a / 3 ) {
+alert ("number is divisible by 3");
+}
+
+task#9) 
+var num = 22;
+for ( i = 0; i < num ; i++);
+var result = ( num / 2);
+if ( result = num) {
+alert("input is even number");
+} else  {
+alert("input is odd number");
+}
+
+task#10)
+var firstNumber = 4;
+var secondNumber = 12;
+
+var a = firstNumber +  secondNumber ;       // 4 + 12 = 16
+var b = firstNumber -  secondNumber ;         // 4 - 12 = 8
+var c = firstNumber *  secondNumber ;         // 4 * 12 = 48
+var d = firstNumber /  secondNumber ;          // 4 / 12 = 3
+var e = firstNumber %  secondNumber ;      // 4 % 12 = 0.33333
+console.log (a);
+console.log (b);
+console.log (c);
+console.log (d);
+console.log (e);
+
+ASSIGNMENT#8   IF... ELSE OR ELSE IF STATEMAENT , TESTING SET OF CONDITION
+
+TASK#1)
+var char = prompt("enter some text");
+char = char. toUperCase();
+var arr = ["MANGO", "APPLE", "GRAPES", "STRAWBERRY"]
+for ( var x = 0; x < arr.lenght; x++)  {
+if( arr[x] === char ) {
+alert ("Text is upper case"); 
+} else ("Text is lower case");
+}
+
+task#2)
+var integer1 = 24;
+var integer2 = -8;
+if ( integer1 < integer2 ) {
+console.log ( "integer2 is greater");
+} else {
+console.log ( "integer1 is greater");
+}
+if (integer1 = integer2 ) {
+console.log(integers are equal);
+{
+
+task#3)
+var num = prompt("enter a number");
+if ( num = +ve number) {
+console.log("positive number");
+}
+if ( num = -ve number) {
+console.log("Negative number");
+} else if ( num = 0) {
+console.log("number is zero");
+}
+
+task#4)
+var a = "a , e, i, o, u";
+var b = i;
+if  ( a.lenght = b) {
+console.log("true");
+} else {
+console.log(""false);
+}
+
+task#5)
+var password = javascript;
+var b = prompt(("enter your password");
+if ( b = null ) {
+console.log("please enter your password");
+}
+if ( b = a) {
+console.log ("Correct! the password you entered matches the orignal answer");
+}  else {
+console.log("Incorrect password");
+}
+
+task#6)
+//    var greeting ;      // not defined
+var hour = 13;
+if (hour < 18) {
+console.log ("Good day")
+} else {
+console.log ("Good evening")
+}
+
+task#7)
+var time= 1900;
+if ( time>= 0000 && time < 1200) {
+console.log("Good morning");
+} 
+if ( time>= 1200 && time < 1700) {
+console.log("Good afternoon");
+} 
+if ( time>= 1700 && time < 2100) {
+console.log("Good evening");
+} 
+if ( time>= 2100 && time <= 2359) {
+console.log("Good night);
+{
+
+ASSIGNMENT#7            ARRAYS
+
+TASK#1)       student name();
+task#2)       var studentName;
+task#3)       var studentName = [" ali, sara, umer"];
+task#4)       var studentAge = [12, 14, 15, 16];
+task#5)      var boolean = [true , false];
+task#6)      var a = [25, true, apple, "hi ali"];
+task#7)     var qualification = [SSC, HSC ,BCS, BS, B.COM, MS, M.PHIL, PHD];
+console.log(qualification);
+
+task#8)      var studentName =  [ "Michael", "john", "tony"];
+var score = [ 320, 230, 480 ];
+var percentage = [ 64%, 46%, 96% ];
+document.write("score of Michael is 320 , percentage : 64%");
+document.write("score of john is 230,  percentage : 46%");
+document.write("score of tony is 480,  percentage : 96%");
+
+task#9)    var colour = [red, yellow, blue, green ];
+a)                 colour(0) = "pink";
+document.write(colour[0]);
+b)                colour(5) = "purple";
+document.write(colour[5]);
+c)                colour.unshift = "white, black";
+document.write(colour);
+d)               colour.shift () ;
+document.write(colour);
+e)               colour.pop () ;
+document.write(colour);
+f)               colour.splice(2, 2, grey, orange) ;
+document.write(colour);
+g)            colour.slice(2, 6 );
+document.write(colour);
+
+task#10)      var score = [320, 230, 480, 120 ] ;
+document.write(score);
+score.sort(function(a,b){return  a-b} ) ;
+document.write(score.sort);
+
+task#11)      var cities = [ karachi, lahore, islamabad, quetta , peshawar]; 
+cities.splice (2, 2);           // isl, quetta
+document.write("cities list :  "  +  cities );
+document.write("selsected cities list  : "  +  cities.splice);
+
+task#12)      var arr = ["This", "is", "my", "cat"] ;
+var string ["  This "+ "  is  "+ "  my  "+ "  cat "] ;
+document.write("Array  : " + arr);
+document.write("String  :  "  +  string);
+
+task#13)      var devices[keyboard, mouse, printer, monitor];
+devices.shift [0] = keyboard;
+devices.shift [1] = mouse;
+devices.shift [2] = printer;
+devices.shift [3] = monitor;
+console.log("Devices  :  " +  devices);
+console.log("Out  : " +   devices.shift [0] );
+console.log("Out  : " +   devices.shift [1] );
+console.log("Out  : " +   devices.shift [2] );
+console.log("Out  : " +   devices.shift [3] );
+
+task#14)      var devices[keyboard, mouse, printer, monitor];
+devices.pop [3] = moniter;
+devices.pop [2] = printer;
+devices.pop [1] = mouse;
+devices.pop [0] = keyboard;
+console.log("Devices  :  " +  devices);
+console.log("Out  : " +   devices.pop [3] );
+console.log("Out  : " +   devices.pop [2] );
+console.log("Out  : " +   devices.pop [1] );
+console.log("Out  : " +   devices.pop [0] );
+
+task#15)    
+var arr = [apple , samsung, motorola, nokia, sony, haier ];
+document.write(" <select> ")
+for ( var i = 0,  i < arr.lenght, i++ )  {
+document.write( " <option> " + arr[ i ]  + " </option> ")
+}
+document.write( " </select> " );
+
+
+
+ASSIGHNMENT# 10       ARRAYS AND LOOPS
+
+task#1)      var fruits = [  [apple][ grapes][ pineapple][ banana ] ];
+
+task#2)
+
+
+task#3)     for (var = 0; i <= 10; i ++) {
+console.log(i);
+}
+
+task#4)    var num = prompt("enter number");
+var lenght = prompt("enter lenght multiplication");
+for (var i = 1; i <=  lenght ; i ++ ) {
+console.log( num  +   "x"   +    i     +     "="     +    num*i    +     "<br>");
+}
+
+task#5)   var fruits = ["Apple", "Banana", "Mango", "Orange", "Strawberry"] ;
+fruits.indexOf [ 0 ];
+fruits.indexOf [ 1 ];
+fruits.indexOf [ 2 ];
+fruits.indexOf [ 3 ];
+fruits.indexOf [ 4 ];
+console.log(fruits);
+console.log( "Element at index 0 is :  " +   fruits.indexOf [ 0 ] );
+console.log( "Element at index 1 is :  " +   fruits.indexOf [ 1 ] );
+console.log( "Element at index 2 is :  " +   fruits.indexOf [ 2 ] );
+console.log( "Element at index 3 is :  " +   fruits.indexOf [ 3 ] );
+console.log( "Element at index 4 is :  " +   fruits.indexOf [ 4 ] );
+
+task#6)    var counting = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 , 13, 14, 15 ;
+var reverse = 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ;
+var even = 0, 2 , 4, 6, 8, 10, 12, 14, 16, 18, 20;
+var odd = 1, 3, 5, 7, 9, 11, 13, 15, 17, 19;
+var series = 2k , 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k;
+document.write("counting :  " +  counting );
+document.write("reverse :  " +  reverse );
+document.write("even :  " +  even);
+document.write("odd :  " +   odd);
+document.write("series :  " +  series );
+
+task#7)    var a = ["apple", "apple pie", "cookies", "chips", "paties"] ;
+var b = prompt("enter bakery items");
+var c = a.indexOf (b);
+if ( b = c) {
+console.log("cookie is available in index : "  +  c   + "in our bakery items");
+} else {
+console.log("item is not found in the list");
+}
+
+task#8)        var a = [24, 53, 78, 91, 12];
+function myArrayMax(a) { 
+console.log("Array items : "   +   a);
+return Math.max.apply(null, a);
+}
+
+task#9)          var a = [24, 53, 78, 91, 12];
+function myArrayMax(a) { 
+console.log("Array items : "   +   a);
+return Math.min.apply(null, a);
+}
+
+task#10)        var i = 5;
+for ( var i = 1; i <= 20; i++ ) {
+console.log(i);
+}
+
+
+
+
+
+
+
